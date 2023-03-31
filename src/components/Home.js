356f43f4decbc81eec5e84d881c9ef6fd02a5d98
Anup Chendake <89442882+anupchendake07@users.noreply.github.com>
2023-03-31 12:15:43 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { Component } from 'react';
+import LocationDisplay from './LocationDisplay';
 
-const Home = () => {
-    return (
-        <div>You are home.</div>
-    )
+export default class Home extends Component {
+	render() {
+		return (
+			<>
+				<div>You are home</div>
+			</>
+		);
+	}
 }
-export default Home;
