bbaa5e38c9c43c27efd2aff011ccef25b7e503f0
Anup Chendake <89442882+anupchendake07@users.noreply.github.com>
2023-03-31 11:17:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,31 @@
-import React, { Component, useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component, useState } from "react";
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
+import NoMatch from "./NoMatch";
 import '../styles/App.css';
-import Navigation from './Navigation';
-import Home from './Home';
-import About from './About';
-import NoMatch from './NoMatch';
-import LocationDisplay from './LocationDisplay';
-
-class AppRouter extends Component {
-	render() {
-		return (
-			<BrowserRouter>
-				<Navigation />
-				<Switch>
-					<Route exact path={'/'} component={Home} />
-					<Route path={'/about'} component={About} />
-					<Route path={'*'} component={NoMatch} />
-				</Switch>
-				<LocationDisplay />
-			</BrowserRouter>
-		);
-	}
-}
+import LocationDisplay from "./LocationDisplay";
 
 class App extends Component {
-	render() {
-		return (
-			<div id="main">
-				<AppRouter />
-			</div>
-		);
-	}
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div id="main">
+                    <LocationDisplay />
+                    <Link to={'/'}>Home</Link>
+                    <Link to={'/about'}>About</Link>
+
+                    <Switch>
+                        <Route exact path={'/'} component={Home} />
+                        <Route exact path={'/about'} component={About} />
+                        <Route exact path={'/*'} component={NoMatch} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        )
+    }
 }
+
 
 export default App;
