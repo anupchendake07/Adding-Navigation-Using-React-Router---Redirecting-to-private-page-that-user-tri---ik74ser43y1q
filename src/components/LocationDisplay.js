9bd37f17e1815ccf76815af5df911af96d643943
Anup Chendake <89442882+anupchendake07@users.noreply.github.com>
2023-04-19 14:25:31 +0530

    Delete LocationDisplay.js

--- src/components/LocationDisplay.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
-
-function LocationDisplay() {
-	const { pathname } = useLocation();
-	return <div data-testid="location-display">{pathname}</div>;
-}
-
-export default LocationDisplay;
